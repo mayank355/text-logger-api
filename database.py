d8feee15dd2928083f3467535fd84d5b24287fdc
mayank355 <qdk.1212@gmail.com>
2026-07-31 13:28:13 +0530

    Fix DATABASE_URL environment variable

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/textlogger"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres123@localhost:5432/textlogger")
 
 engine = create_engine(DATABASE_URL)
 
